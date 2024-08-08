762e6d1d9cb0f06593f51216c1a1dd91e7edd227
N. <42706907+iamnoksio@users.noreply.github.com>
2024-08-08 14:36:21 -0400

    Don't create object and create overhead at every hit

--- nPaper-Server/src/main/java/net/minecraft/server/EntityLiving.java
+++ nPaper-Server/src/main/java/net/minecraft/server/EntityLiving.java
@@ -1735,8 +1735,8 @@ public abstract class EntityLiving extends Entity {
     }
 
     public boolean hasLineOfSight(Entity entity) {
-    	final Vec3D vec = new Vec3D(this.locX, this.locY + (double) this.getHeadHeight(), this.locZ);
-        return this.world.rayTrace(vec, new Vec3D(entity.locX, entity.locY + (double) this.getHeadHeight(), entity.locZ)) == null;
+    	final Vec3D vec = Vec3D.a(this.locX, this.locY + (double) this.getHeadHeight(), this.locZ);
+        return this.world.rayTrace(vec, Vec3D.a(entity.locX, entity.locY + (double) this.getHeadHeight(), entity.locZ)) == null;
     }
 
     public Vec3D ag() {
