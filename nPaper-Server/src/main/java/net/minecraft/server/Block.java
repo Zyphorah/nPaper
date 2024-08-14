d5205f39e306601315f7f5013073f8dff8d98159
N. <42706907+iamnoksio@users.noreply.github.com>
2024-08-13 22:37:23 -0400

    Use MathHelper for Block::range

--- nPaper-Server/src/main/java/net/minecraft/server/Block.java
+++ nPaper-Server/src/main/java/net/minecraft/server/Block.java
@@ -826,13 +826,7 @@ public class Block {
 
     // Spigot start
     public static float range(float min, float value, float max) {
-        if (value < min) {
-            return min;
-        }
-        if (value > max) {
-            return max;
-        }
-        return value;
+        return MathHelper.limit(value, min, max);
     }
     // Spigot end
 }
